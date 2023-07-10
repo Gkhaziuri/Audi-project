16124e8523cad28e6eb111fd12e27a57dd208b1e
Giorgi Khaziuri <gxaziuri7@gmail.com>
2023-07-11 00:04:06 +0400

    added rs7 page

--- faq.js
+++ faq.js
@@ -5,11 +5,11 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.height === "100px") {
-      panel.style.height = "0px";
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
 
     } else {
-      panel.style.height = "100px"
+      panel.style.display = "block"
  
     }
   });
